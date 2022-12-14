92a8daf5a53b8576a5fb70b6cee9f7c019b3ae46
kubat <kubat.aiupov@gmail.com>
2022-12-14 21:13:54 +0100

    add pop and removeFirstItem functions

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ const emojisArray = [
   "😚",
 ];
 
-let arr = [1,2,3]
 // pushs items to an array
 const addItem = (i) => {
   return items.push(emojisArray[i])
@@ -50,17 +49,35 @@ const randomIndex = () => Math.floor(Math.random() * emojisArray.length);
 // adds items to the the beginning of the array
 const addItemToBegin = (i) => items.unshift(emojisArray[i]);
 
-// insert items to specific index
-const insertItem = ((arr, index) => arr.splice(index, 0, emojisArray[randomIndex()]));
+// insert items to specific index // should be revised
+const insertItem = ((index, i) => items.splice(index, 0, i));
+
+// removes last item from the array
+const popItem = () => items.pop();
+
+// removes first item from the array
+const removeFirstItem = () => items.shift();
+
 
 
 randomIndex();
 
-addItemToBegin(randomIndex());
-addItem(randomIndex());
+//addItemToBegin(randomIndex());
+//addItem(randomIndex());
 
-//items.forEach(i => console.log(i))
-console.log(insertItem(arr,0))
+items.forEach(i => console.log(i))
+//popItem();
+//removeFirstItem();
+//console.log(items)
+//console.log(insertItem(0,0,2))
+
+for (let i = 0; i <= 6; i++) {
+  addItem(i);
+}
+console.log(removeFirstItem())
+console.log(items)
+
+
 
 
 
